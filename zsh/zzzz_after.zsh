51ee0c2dfb763e4805e0062c9a96f6d11f1fdfaf
Kieran Graham <me@kierangraham.com>
2016-03-28 12:39:48 +0100

    explicit rbenv path

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -5,6 +5,6 @@ if [ -d $HOME/.zsh.after/ ]; then
   fi
 fi
 
-eval "$(rbenv init -)"
+eval "$(/usr/local/bin/rbenv init -)"
 
 export PATH=./bin:node_modules/.bin:/usr/local/bin:/usr/local/sbin:$PATH
